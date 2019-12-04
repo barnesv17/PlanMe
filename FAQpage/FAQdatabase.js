d7dda22bf8d65f2620188093e3dc42056488ed85
KennethLee8 <kennethleeje8@gmail.com>
2019-12-03 19:52:02 -0500

    commit

--- FAQpage/FAQdatabase.js
+++ FAQpage/FAQdatabase.js
@@ -1,30 +1,30 @@
 { "items": [
 	   {
-       "question": "How do you contact the venue owner?"
+       "question": "How do you contact the venue owner?",
        "answer": "We provide the phone number and email of each venue owner on each venue"
      },
      {
-       "question": "How do I pay the venue owner to rent out the venue?"
+       "question": "How do I pay the venue owner to rent out the venue?",
        "answer": "Payments are not handled by us, so you would have to make an arrangement with the "
      },
      {
-       "question": "Are the prices of the venues fixed?"
+       "question": "Are the prices of the venues fixed?",
        "answer": "The price of the venue will change depending on traffic on the venue and the extra options avaliable"
      },
      {
-       "question": "How do I check if a venue is booked or not?"
+       "question": "How do I check if a venue is booked or not?",
        "answer": "The date will not be able to be selected. Another options is to contact the venue owner themselves for further inquiry"
      },
      {
-       "question": "What is the deposit for?"
+       "question": "What is the deposit for?",
        "answer": "The deposit for the venues is to cover the costs of repairing in the case that property is destroyed"
      },
      {
-       "question": "Is the venue refundable?"
+       "question": "Is the venue refundable?",
        "answer": "There is a 48 hour notice policy and you will be fully refunded in the event you cancel before this time"
      },
      {
-       "question": "Can I use external options for extra services provided"
+       "question": "Can I use external options for extra services provided",
        "answer": "Yes, the options provided are just for convenience, however, the venue owners will have to notified of this"
      }
    ]}
