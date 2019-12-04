1d6520b5fafcf6c2ea923224fb936496a46074e6
KennethLee8 <kennethleeje8@gmail.com>
2019-12-03 21:39:01 -0500

    commit

--- venuepage/VenuePage.js
+++ venuepage/VenuePage.js
@@ -1,17 +1,34 @@
+// Taken from stackoverflow
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
 $(document).ready(function() {
-  
-    $.ajax({
-      type: "GET",
-      url: "../resources/database.json",
-      dataType: "json",
-      success: function(responseData, status){
-        var output = "";  
-        $.each(responseData.items, function(i, item) {
+
+
+  var venueName = getParameterByName('Title');
+
+  $.ajax({
+    type: "GET",
+    url: "../resources/database.js",
+    dataType: "json",
+    success: function(responseData, status){
+      var output = "";
+      $.each(responseData.items, function(i, item) {
+        if ( item.title == venueName ) {
+
+          new_title = item.title.replace(/_/g, ' ');
+
           output += '<div class="column">';
           output += '<div class="content">';
+          output += '<h1>'+ new_title +'</h1>';
           output += '<img src="'+item.media+'" alt="'+item.title+'" style="100%"">';
-          output += '<h3>'+ item.title +'</h3>';
           output += '<p>';
           output += 'Price: ' + item.price;
           output += 'Address: ' + item.address;
@@ -20,13 +37,12 @@ $(document).ready(function() {
           output += '</p>';
           output += '</div>';
           output += '</div>';
-        });
-        
-        $('#row').html(output);
-      }, error: function(msg) {
-              // there was a problem
-        alert("There was a problem: " + msg.status + " " + msg.statusText);
-      }
-    });
+        }
+      });
+      $('.venueOutput').html(output);
+    }, error: function(msg) {
+            // there was a problem
+      alert("There was a problem: " + msg.status + " " + msg.statusText);
+    }
   });
-   
+});
