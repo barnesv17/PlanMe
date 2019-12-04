06068a5b06c806b25e7716dc10990605aba2e389
KennethLee8 <kennethleeje8@gmail.com>
2019-12-03 20:50:51 -0500

    commit

--- FAQpage/FAQpage.js
+++ FAQpage/FAQpage.js
@@ -10,11 +10,12 @@ $(document).ready(function() {
    	 	dataType: "json",
    	 	success: function(responseData, status){
    	  	var output = "";
-    	 	$.each(responseData.items, function(i, item) {
+    	 	$.each(responseData.items, function(i, item) { //searching through each item in the database of FAQ (FAQdatabase.js)
        		output += '<p2>' + item.question + '<br>' + item.answer + '<br>' + '</p2>' + '<hr>';
+          //code that is going to go into the html file and create the format needed
       	});
-      	$('#questionAnswer').html(output);
-    	}, error: function(msg) {
+      	$('#questionAnswer').html(output); //outputs the html code to the div block ID questionAnswer in FAQpage.html
+    	}, error: function(msg) { //error checking to ensure it is running smoothlys
       				// there was a problem
       	alert("There was a problem: " + msg.status + " " + msg.statusText);
     	}
